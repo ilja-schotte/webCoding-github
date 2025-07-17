5ced6421163b9c93ef38e8e1c0e5e0d5f2ae9bc5
Ilja Schotte <i.schotte@posteo.de>
2025-07-17 07:04:30 +0200

    weiter gemacht

--- fotoclub/js/main.js
+++ fotoclub/js/main.js
@@ -1,7 +1,21 @@
 "use strict";
 
-$(document).ready(function(){
-
-    console.log('nice to see you')
+let t1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: 'main',
+        start: "500",
+        scroller: "main"
+    },
 });
+
+let t2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: 'main',
+        start: "500",
+        scroller: "main"
+    },
+});
+
+t1.from('#about_darkbox',{ x: 100, y: 100, opacity:0, duration:1})
+t2.from('#about_whitebox',{ x: -100, y: -100, opacity:0, duration:1})
 
