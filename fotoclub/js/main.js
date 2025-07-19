73e21a20536bac0887e0b17135a608b11082d7cc
Ilja Schotte <i.schotte@posteo.de>
2025-07-19 17:40:48 +0200

    further improvements

--- fotoclub/js/main.js
+++ fotoclub/js/main.js
@@ -1,5 +1,70 @@
 "use strict";
 
+
+/* ========================== Navigation ========================== */
+function addClickEventsToMenuItems() {
+
+    /*
+    Hinzufügen eines ScrollTo Eventlistener für jeden Eintrag im Menü
+    um bei 'click' zur entsprechenden Section zu scrollen.
+    */
+
+    const navMenuItems = document.querySelectorAll('.navMenuItem');
+    const main = document.querySelectorAll('main');
+
+    navMenuItems.forEach((navMenuItem, idx) => {
+        navMenuItem.addEventListener('click', () => {
+            gsap.to(main, {
+                duration: 1,
+                scrollTo: `#${navMenuItem.dataset.sectionname}`
+            });
+        });
+    });
+}
+addClickEventsToMenuItems();
+
+// ================================================================
+function changeStyleOfNavigationBar() {
+    
+    /*
+    Wenn die landing-section verlassen wird, soll sich der Stil
+    der Navigationbar ändern.
+    */
+
+    const header = document.querySelector('header');
+    const landing = document.querySelector('#landing');
+    const navMenuItems = document.querySelectorAll('.navMenuItem');
+    const landingSectionOptions = {};
+    const landingSectionObserver = new IntersectionObserver(function (
+        entries,
+        landingSectionObserver
+    ){
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                header.classList.remove('dark-mode');
+                navMenuItems.forEach(navMenuItem => {
+                    navMenuItem.classList.remove('dark-mode');
+                });
+            }
+            else {
+                header.classList.add('dark-mode');
+                navMenuItems.forEach(navMenuItem => {
+                    navMenuItem.classList.add('dark-mode');
+                });
+            }
+        });
+    },landingSectionOptions);
+    landingSectionObserver.observe(landing);
+}
+changeStyleOfNavigationBar();
+
+
+// ================================================================
+
+
+
+/* ============================ Über uns ========================== */
+// Hintergrundanimation
 let t1 = gsap.timeline({
     scrollTrigger: {
         trigger: 'main',
@@ -16,6 +81,5 @@ let t2 = gsap.timeline({
     },
 });
 
-t1.from('#about_darkbox',{ x: -100, y: -100, opacity:0, duration:1})
-t2.from('#about_whitebox',{ x: 100, y: 100, opacity:0, duration:1})
-
+t1.from('#about_darkbox', { x: -100, y: -100, opacity: 0, duration: 1 });
+t2.from('#about_whitebox', { x: 100, y: 100, opacity: 0, duration: 1 });
