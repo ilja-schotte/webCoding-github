9a55fde45f476cb4f685864ce7b4759d7ac2ca4b
Ilja Schotte <i.schotte@posteo.de>
2025-08-24 07:11:14 +0200

    further improvements

--- fotogalerie/js/main.js
+++ fotogalerie/js/main.js
@@ -1,41 +1,68 @@
 "use strict";
 
+$(document).ready(() => {
 
+    var myPhotoGallerySwiper = Object();
+    var myPhotoGalleryPreviewSwiper = Object();
 
+    // ================================================================
+    var initializePhotogallerySwiperJS = () => {
 
+        myPhotoGalleryPreviewSwiper = new Swiper(".myPhotoGalleryPreviewSwiper", {
+            loop: true,
+            spaceBetween: 10,
+            slidesPerView: 1,
+            allowTouchMove: false,
+            freeMode: true,
+            watchSlidesProgress: false,
+            loop: true
+        });
+        myPhotoGallerySwiper = new Swiper(".myPhotoGallerySwiper", {
+            loop: true,
+            spaceBetween: 10,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            thumbs: {
+                swiper: myPhotoGalleryPreviewSwiper,
+            },
+        });
+    }
+    initializePhotogallerySwiperJS();
 
-// ================================================================
-function initializePhotogallerySwiperJS() {
+    // ================================================================
+    var openLightboxByClickingImage = () => {
 
-    var swiper = new Swiper(".myPhotoGalleryPreviewSwiper", {
-        loop: true,
-        spaceBetween: 10,
-        slidesPerView: 8,
-        freeMode: true,
-        watchSlidesProgress: true,
-    });
-    var swiper2 = new Swiper(".myPhotoGallerySwiper", {
-        loop: true,
-        spaceBetween: 10,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        thumbs: {
-            swiper: swiper,
-        },
-    });
+        // Modal Window
+        const lightbox = document.getElementById("lightbox");
 
-}
-initializePhotogallerySwiperJS();
+        // get all image-cards
+        const allImageCards = document.querySelectorAll('.gallery-image-card');
 
+        allImageCards.forEach((imageCard, idx) => {
+            imageCard.addEventListener('click', () => {
+                lightbox.style.display = 'flex';
+            });
+        });
+    }
+    openLightboxByClickingImage();
 
-// ================================================================
-function closeGalleryLightbox(){
+    // ================================================================
+    var closeLightboxByClickingX = () => {
 
-    const lightbox = document.getElementById('lightbox');
+        // x-Button:
+        const xButton = document.getElementById('lightbox-preview-close');
+        // Modal Window
+        const lightbox = document.getElementById("lightbox");
 
-    
+        // Füge Eventlistener hinzu:
+        xButton.addEventListener('click', () => {
 
-}
-closeGalleryLightbox();
+            // close Window
+            lightbox.style.display = 'none';
+        });
+    }
+    closeLightboxByClickingX();
+
+});
