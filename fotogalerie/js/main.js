3cab82ad2ddf797ae1528d1640bc7cca69fc7ca4
Ilja Schotte <i.schotte@posteo.de>
2025-08-26 16:39:23 +0200

    gallery slider almost finished

--- fotogalerie/js/main.js
+++ fotogalerie/js/main.js
@@ -2,37 +2,68 @@
 
 $(document).ready(() => {
 
-    var myPhotoGallerySwiper = Object();
-    var myPhotoGalleryPreviewSwiper = Object();
+    /* Swiper für die Darstellung der Bilder */
+    //var myPhotoGalleryPreviewSwiper = Object();
+
+    /* Swiper für die Vorschaubilder */
+    //var myPhotoGalleryPreviewThumbsSwiper = Object();
+
 
     // ================================================================
     var initializePhotogallerySwiperJS = () => {
 
-        myPhotoGalleryPreviewSwiper = new Swiper(".myPhotoGalleryPreviewSwiper", {
+        var myPhotoGalleryPreviewThumbsSwiper = new Swiper(".myPreviewThumbsSwiper", {
+            slidesPerView: 2,
+            spaceBetween: 10,
             loop: true,
+            freeMode: false,
+            centeredSlides: false,
+            breakpoints: {
+                // when wondow width is >= 400px
+                400: {
+                    slidesPerView: 3,
+                    spaceBetween: 10,
+                },
+                // when wondow width is >= 800px
+                800: {
+                    slidesPerView: 4,
+                    spaceBetween: 10,
+                },
+                1024: {
+                    slidesPerView: 5,
+                    spaceBetween: 10,
+                },
+            },
+            /*on: {
+                click(event) {
+                    console.log('event.target', this.clickedIndex);
+                    myPhotoGalleryPreviewThumbsSwiper.slideTo(this.clickedIndex);
+                },
+            }*/
+        });
+        var myPhotoGalleryPreviewSwiper = new Swiper(".myPreviewSwiper", {
             spaceBetween: 10,
             slidesPerView: 1,
-            allowTouchMove: false,
-            freeMode: true,
-            watchSlidesProgress: false,
-            loop: true
-        });
-        myPhotoGallerySwiper = new Swiper(".myPhotoGallerySwiper", {
             loop: true,
-            spaceBetween: 10,
             navigation: {
                 nextEl: ".swiper-button-next",
                 prevEl: ".swiper-button-prev",
             },
             thumbs: {
-                swiper: myPhotoGalleryPreviewSwiper,
+                swiper: myPhotoGalleryPreviewThumbsSwiper,
             },
         });
     }
     initializePhotogallerySwiperJS();
 
+
+
+
     // ================================================================
     var openLightboxByClickingImage = () => {
+
+        // get swiper
+        const myPreviewSwiper = document.querySelector('.myPreviewSwiper').swiper;
 
         // Modal Window
         const lightbox = document.getElementById("lightbox");
@@ -41,12 +72,18 @@ $(document).ready(() => {
         const allImageCards = document.querySelectorAll('.gallery-image-card');
 
         allImageCards.forEach((imageCard, idx) => {
-            imageCard.addEventListener('click', () => {
+            imageCard.addEventListener('click', (e) => {
+
                 lightbox.style.display = 'flex';
+                myPreviewSwiper.slideToLoop(parseInt(e.target.parentElement.dataset.imageindex), 1);
+                myPreviewSwiper.update();
             });
         });
     }
     openLightboxByClickingImage();
+
+
+
 
     // ================================================================
     var closeLightboxByClickingX = () => {
